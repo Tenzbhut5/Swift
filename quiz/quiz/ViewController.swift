8efec5e2345812154239b7f3837d6c210c1052c6
Tenzbhut5 <41802997+Tenzbhut5@users.noreply.github.com>
2018-07-27 14:13:26 -0400

    Add files via upload

---
+++ quiz/quiz/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Spence on 7/24/18.
+//  Copyright © 2018 Spence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func opyion1(_ sender: UIButton) {
+        
+    }
+    
+    @IBOutlet weak var option2: UIButton!
+    
+    @IBOutlet weak var option3: UIButton!
+    
+    @IBOutlet weak var option4: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
